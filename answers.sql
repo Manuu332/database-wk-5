e4b16c7c5547669187f0515a285048a1bbaf3f04
Emmanuel Oyuga <117825678+Manuu332@users.noreply.github.com>
2025-10-22 16:10:26 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,15 @@
+-- Question 1
+ALTER TABLE customers
+DROP INDEX IdxPhone;
+-- Question 2
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 3
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+GRANT SELECT ON salesDB.products TO 'bob'@'localhost';
+
+-- 3. Apply privilege changes
+FLUSH PRIVILEGES;
+
+-- Question 4 
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
