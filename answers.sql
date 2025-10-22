9512e3027a312910e1c79711ee57a5b298e882dd
Emmanuel Oyuga <117825678+Manuu332@users.noreply.github.com>
2025-10-22 16:12:15 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -7,9 +7,7 @@ CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
 -- Question 3
 GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
 GRANT SELECT ON salesDB.products TO 'bob'@'localhost';
-
--- 3. Apply privilege changes
-FLUSH PRIVILEGES;
+  FLUSH PRIVILEGES;
 
 -- Question 4 
 ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
